ee5d4dde1179978193a4ef26b7b1b63fa490f40e
amazigh Bouldjenet <sc20amb@leeds.ac.uk>
2023-03-15 22:17:38 +0000

    defined move class functions

--- include/move.h
+++ include/move.h
@@ -10,23 +10,23 @@ enum class MoveType {
     KingRookMoveH
 };
 struct Move {
-public:
-    Position start_;
-    Position end_;
-    PieceColor color_;
-    PieceType type_;
-    MoveType move_type_;
-    std::size_t AsNumber() const {
-        return start_.AsNumber() * 64 + end_.AsNumber();
-    }
-    bool operator==(const Move& other) const {
-        return start_ == other.start_ && end_ == other.end_ && color_ == other.color_ && type_ == other.type_;
-    }
+ public:
+  Position start_;
+  Position end_;
+  PieceColor color_;
+  PieceType type_;
+  MoveType move_type_;
+  std::size_t AsNumber() const { 
+       return start_.AsNumber() * 64 + end_.AsNumber();
+  }
+  bool operator==(const Move& other) const {
+    return start_ == other.start_ && end_ == other.end_ && color_ == other.color_ && type_ == other.type_;
+  }
 };
 
 template <>
 struct std::hash<Move> {
-    std::size_t operator()(const Move& move) const noexcept {
-        return move.AsNumber();
-    }
+  std::size_t operator()(const Move& move) const noexcept {
+    return move.AsNumber();
+  }
 };
